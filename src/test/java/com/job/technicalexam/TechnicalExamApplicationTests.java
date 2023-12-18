bb595be0c6802c57b71ca71b2b865bf38532fd66
Joshua Cariño <jcarino@mycwt.com>
2023-12-19 00:20:20 +0800

    First Commit

---
+++ src/test/java/com/job/technicalexam/TechnicalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.technicalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
